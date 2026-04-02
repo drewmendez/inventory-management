1a2d595c8d022cddb2601eedafb916f42c957d2b
Andrew Jefferson Mendez <andrewjeffmendez30@gmail.com>
2026-04-02 21:20:21 +0800

    feat(api): add MeController to return current authenticated user

--- apps/api/routes/api.php
+++ apps/api/routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\MeController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
-    // TODO: Add API routes here
+    Route::get('/me', MeController::class);
 });
