618ff4e88317a3205639b2e475be4dda3f647e1a
Andrew Jefferson Mendez <andrewjeffmendez30@gmail.com>
2026-04-02 15:44:32 +0800

    feat: setup migration tables

---
+++ apps/api/database/migrations/2026_04_02_072646_create_transactions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('reference_number')->unique();
+            $table->smallInteger('type')->comment('1: STOCK IN, 2: STOCK OUT');
+            $table->string('remarks')->nullable();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
---
+++ apps/api/database/migrations/2026_04_02_073348_create_inventory_movements_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventory_movements', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('from_quantity', 10, 2);
+            $table->decimal('to_quantity', 10, 2);
+            $table->foreignId('transaction_item_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inventory_movements');
+    }
+};
